00bc69ef6237f3edef76a69aa2962a0e6ce297a5
popshia <B123292179@gmail.com>
2019-04-04 23:19:54 +0800

    brainstorming:(((

--- 2-3_Tree&AVL_Tree/main.cpp
+++ 2-3_Tree&AVL_Tree/main.cpp
@@ -51,33 +51,33 @@ public:
             else return rightDepth+1 ;
         } //else
     } // Get tree height
-    
+
     void GetData() {
         DataStruct tempData ;
         string temp = "\0" ;
         getline( input, temp ) ; // read the labels in the first three lines
         getline( input, temp ) ;
         getline( input, temp ) ;
-        
+
         while ( getline( input, temp ) ) {
             tempData.whole = temp ;
             tempData.index = Count ;
-            
+
             vector<string> tokens ;
             istringstream cutStream( temp ) ;
-            
+
             while ( getline( cutStream, temp, '\t' ) ) // cut the token one by one
                 tokens.push_back( temp ) ;
-            
+
             tempData.schoolName = tokens[1] ;
             tempData.departmentName = tokens[3] ;
             tempData.dayOrNight = tokens[4] ;
             tempData.graduateType = tokens[5] ;
             tempData.student = atoi( tokens[6].c_str() ) ;
-            // InsertBySchoolName( tempdata ) ; // Insert & Analyze
+            InsertBySchoolName( tempData ) ; // Insert & Analyze
             Count++ ;
         } // get the whole file
-        
+
         cout << "Tree Height = " << GetHeight( root ) << endl ; // print the tree height
         int n = 1 ;
         for ( int first = 0 ; first < root->nodeData.size() ; first++ ) {
@@ -93,27 +93,119 @@ public:
         } // print the first layer
         cout << endl ;
     } // Analyze the whole input file
-    
+
     void InsertBySchoolName( DataStruct data ) {
         PointerStruct* walk = root ;
+        vector<DataStruct> tempVector ;
+        bool notYet = true ;
+
         if ( walk == NULL ) {
             walk = new PointerStruct ;
             walk->nodeData[0].push_back( data ) ;
+            walk->parent = root ;
             root = walk ;
         } // first data
+
         else {
-            while ( walk != NULL ) {
-                
-            }
-        }
-    }
-    
-    void AnalyzeBySchoolName() {
-        
-    }
-    
-    void FixBySchoolName() {
-        
+            while ( notYet == true ) {
+                if ( walk->nodeData.size() == 1 ) {
+                    if ( data.schoolName > walk->nodeData[0][0].schoolName ) {
+                        if ( walk->rightChild == NULL ) {
+                            tempVector.push_back( data ) ;
+                            walk->nodeData.push_back( tempVector ) ;
+                            notYet = false ;
+                        } // if
+
+                        else {
+                            walk->parent = walk ;
+                            walk = walk->rightChild ;
+                        } // else
+                    } // greater than
+
+                    else if ( data.schoolName < walk->nodeData[0][0].schoolName ) {
+                        if ( walk->leftChild == NULL ) {
+                            tempVector.push_back( data ) ;
+                            walk->nodeData.insert( walk->nodeData.begin(), tempVector ) ;
+                            notYet = false ;
+                        } // if
+
+                        else {
+                            walk->parent = walk ;
+                            walk = walk->leftChild ;
+                        } // else
+                    } // smaller than
+
+                    else if ( data.schoolName == walk->nodeData[0][0].schoolName ) {
+                        tempVector.push_back( data ) ;
+                        walk->nodeData[0].push_back( data ) ;
+                        notYet = false ;
+                    } // equal to
+                } // size = 1
+
+                else {
+                    if ( data.schoolName == walk->nodeData[0][0].schoolName ) {
+                        walk->nodeData[0].push_back( data ) ;
+                        notYet = false ;
+                    } // equal to the first data in walk
+
+                    else if ( data.schoolName == walk->nodeData[1][0].schoolName ) {
+                        walk->nodeData[1].push_back( data ) ;
+                        notYet = false ;
+                    } // equal to the second data in walk
+
+                    else if ( data.schoolName > walk->nodeData[0][0].schoolName && data.schoolName < walk->nodeData[1][0].schoolName ) {
+                        walk->parent = walk ;
+                        walk = walk->middleChild ;
+                        tempVector.push_back( data ) ;
+                        walk->nodeData.insert( walk->nodeData.begin()+1, tempVector ) ;
+                        notYet = false ;
+                    } // in the middle of the two data in walk
+
+                    else if ( data.schoolName < walk->nodeData[0][0].schoolName ) {
+                        if ( walk->leftChild == NULL ) {
+                            tempVector.push_back( data ) ;
+                            walk->nodeData.insert( walk->nodeData.begin(), tempVector ) ;
+                            notYet = false ;
+                        } // if
+
+                        else {
+                            walk->parent = walk ;
+                            walk = walk->leftChild ;
+                        } // else
+                    } // the smallest of two data in walk
+
+                    else if ( data.schoolName > walk->nodeData[1][0].schoolName ) {
+                        if ( walk->rightChild == NULL ) {
+                            tempVector.push_back( data ) ;
+                            walk->nodeData.push_back( tempVector ) ;
+                            notYet = false ;
+                        } // if
+
+                        else {
+                            walk->parent = walk ;
+                            walk = walk->rightChild ;
+                        } // else
+                    } // the greatest of two data in walk
+                } // size = 2
+            } // while not found the right place
+
+            if ( walk->nodeData.size() > 2 ) FixBySchoolName( walk->parent, walk ) ;
+        } // not first data
+    } // InsertBySchoolName( )
+
+    void FixBySchoolName( PointerStruct* parent, PointerStruct* currentIssue ) {
+        PointerStruct* newNode ;
+        vector<DataStruct> tempNewVector ;
+
+        if ( parent->nodeData.size() < 2 ) {
+            if ( currentIssue->nodeData[1][0].schoolName < parent->nodeData[0][0].schoolName )
+                parent->nodeData.insert( parent->nodeData.begin(), currentIssue->nodeData[1] ) ;
+            else if ( currentIssue->nodeData[1][0].schoolName > parent->nodeData[0][0].schoolName )
+                parent->nodeData.push_back( currentIssue->nodeData[1] ) ;
+            currentIssue->nodeData.erase( currentIssue->nodeData.begin()+1 ) ;
+        } // parent is not full
+
+        else if (  )
     }
 } ;
 
@@ -121,15 +213,11 @@ class AVLTree : public TwoThreeTree {
 private:
 public:
     void InsertByDepartmentName( DataStruct data ) {
-        
+
     }
-    
-    void AnalyzeByDepartmentName() {
-        
-    }
-    
+
     void FixByDepartmentName() {
-        
+
     }
 } ;
 
@@ -137,7 +225,7 @@ int main() {
     bool continueOrNot = false ;
     TwoThreeTree twoThreeTree ;
     AVLTree avlTree ;
-    
+
     do {
         cout << "**********************************************" << endl ; // welcome message
         cout << "*****         2-3 Tree & AVL Tree        *****" << endl ;
@@ -146,32 +234,32 @@ int main() {
         cout << "***** 2 : Build AVL Tree                 *****" << endl ;
         cout << "**********************************************" << endl ;
         cout << endl << "Please enter your choice:" << endl ;
-        
+
         cin >> Command ; // read in user command
         cout << endl ;
-        
+
         if ( Command == 0 ) { // bye :(((
             cout << "Bye :(((" << endl ;
             return 0 ;
         } // quit
-        
+
         else if ( Command > 2 || Command < 0 ) {
             cout << "Error command! please enter an acceptable command:" << endl << endl ;
             continueOrNot = true ;
         } // wrong command
-        
+
         else if ( Command == 1 ) { // read, count and build
             bool function1Confirm = false ;
-            
+
             do {
                 cout << "Please enter the file you want to build a 2-3 Tree or [0] to quit:" << endl ;
                 cin >> FileNumber ;
-                
+
                 if ( FileNumber == "0" ) {
                     function1Confirm = true ;
                     continueOrNot = true ;
                 } // quit
-                
+
                 else {
                     string fileName = "input" + FileNumber + ".txt" ;
                     input.open( fileName.c_str() ) ;
@@ -183,25 +271,25 @@ int main() {
                     else cout << "*****  " << fileName << " does not exist!  *****" << endl ;
                 } // continue
             } while( ! function1Confirm ) ;
-            
+
             Count = 1 ;
             FileNumber = "0" ;
             input.close() ;
             output.close() ;
         } // 2-3 tree
-        
+
         else if ( Command == 2 ) {
             bool function2Confirm = false ;
-            
+
             do {
                 cout << "Please enter the file you want to build a AVL Tree or [0] to quit:" << endl ;
                 cin >> FileNumber ;
-                
+
                 if ( FileNumber == "0" ) {
                     function2Confirm = true ;
                     continueOrNot = true ;
                 } // quit
-                
+
                 else {
                     string fileName = "input" + FileNumber + ".txt" ;
                     input.open( fileName.c_str() ) ;
@@ -213,7 +301,7 @@ int main() {
                     else cout << "*****  " << fileName << " does not exist!  *****" << endl ;
                 } // open file and input data to BST
             } while ( ! function2Confirm ) ;
-            
+
             Count = 0 ;
             FileNumber = "0" ;
             input.close() ;
